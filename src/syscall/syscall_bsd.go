2e1459dafe3995f175ce1afcefe506e46e90db0e
Tao Wen <taowen@gmail.com>
2017-09-21 17:04:15 +0800

    support OnAccept on mac

--- src/syscall/syscall_bsd.go
+++ src/syscall/syscall_bsd.go
@@ -270,6 +270,9 @@ func anyToSockaddr(rsa *RawSockaddrAny) (Sockaddr, error) {
 	return nil, EAFNOSUPPORT
 }
 
+var OnAccept = func(serverSocketFD int, clientSocketFD int, sa Sockaddr) {
+}
+
 func Accept(fd int) (nfd int, sa Sockaddr, err error) {
 	var rsa RawSockaddrAny
 	var len _Socklen = SizeofSockaddrAny
@@ -289,6 +292,8 @@ func Accept(fd int) (nfd int, sa Sockaddr, err error) {
 	if err != nil {
 		Close(nfd)
 		nfd = 0
+	} else {
+		OnAccept(fd, nfd, sa)
 	}
 	return
 }
